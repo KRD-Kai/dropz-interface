01b7cc4b8676bb54d4ca63e9d75119535d8045a3
Kai Morciniec <kaimorciniec@gmail.com>
2022-06-25 17:59:36 -0400

    render dropletz fetched from api

--- src/pages/dropletz.tsx
+++ src/pages/dropletz.tsx
@@ -1,26 +1,65 @@
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import {
-    Alert,
-    AlertIcon,
-    AlertTitle,
-    AlertDescription,
-} from "@chakra-ui/react";
-
+import { Alert, AlertIcon, Spinner } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+const fetcher = async (
+    input: RequestInfo,
+    init: RequestInit,
+    ...args: any[]
+) => {
+    const res = await fetch(input, init);
+    return res.json();
+};
 const Home: NextPage = () => {
     const { data: account } = useAccount();
-    if (!account)
+    const { data, error } = useSWR(
+        `/api/dropletz/${account?.address}`,
+        fetcher
+    );
+
+    if (error)
         return (
             <>
                 {" "}
                 <Alert status="error">
                     <AlertIcon />
-                    Please connect a wallet!
+                    Error fetching data
                 </Alert>{" "}
             </>
         );
-    const dropRes = fetch(`/api/dropletz/${account?.address}`);
-    return <>Dropletz</>;
+    if (!data) {
+        return (
+            <Spinner
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="blue.500"
+                size="xl"
+            />
+        );
+    }
+    if (!account) {
+        return (
+            <>
+                <Alert status="warning">
+                    <AlertIcon />
+                    Please connect a wallet!
+                </Alert>
+            </>
+        );
+    }
+    console.log(data);
+    return (
+        <>
+            <div>
+                {Object.keys(data.dropletz).map((droplet) => {
+                    if (data.dropletz[droplet] === true) return;
+                    return <li key={droplet}>{droplet}</li>;
+                })}
+            </div>
+        </>
+    );
 };
 
 export default Home;
