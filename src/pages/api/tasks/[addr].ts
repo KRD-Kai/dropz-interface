2271aa0fff7bcd73453ccaf4a219e309cab98bd9
Kai Morciniec <kaimorciniec@gmail.com>
2022-06-25 00:43:57 -0400

    add basic dynamic api route

---
+++ src/pages/api/tasks/[addr].ts
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    let { addr } = req.query;
+    res.status(200).json({ address: addr });
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Layout from "../components/layout/layout";
+import Layout from "../components/layout/Layout";
 
 const Home: NextPage = () => {
     return <Layout>App</Layout>;
