accd34d2ef56604d13fe89d7664b3e78a9d94719
Kai Morciniec <kaimorciniec@gmail.com>
2022-06-25 01:30:43 -0400

    add ens task check to api

--- src/pages/api/tasks/[addr].ts
+++ src/pages/api/tasks/[addr].ts
@@ -1,7 +1,47 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import "dotenv/config";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
     let { addr } = req.query;
-    res.status(200).json({ address: addr });
+    let tasks = {
+        ownsEns: false,
+    };
+    const domains = await getENSDomains(addr);
+    if (domains?.length > 0) tasks.ownsEns = true;
+
+    res.status(200).json({ tasks: tasks });
+}
+
+async function getENSDomains(addr) {
+    const response = await fetch(
+        `https://gateway.thegraph.com/api/${process.env.GRT_APIKEY}/subgraphs/id/EjtE3sBkYYAwr45BASiFp8cSZEvd1VHTzzYFvJwQUuJx`,
+        {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                query: `
+            query {
+                accounts(where: {id: "${addr.toLowerCase()}"}) {
+                    registrations {
+                        id
+                        domain {
+                            name
+                            labelName   
+                            id
+                        }
+                    }
+                }
+            }`,
+            }),
+        }
+    );
+    const responseBody = await response.json();
+    const domains = responseBody.data.accounts[0]?.registrations;
+    console.log(domains);
+    return domains;
 }
