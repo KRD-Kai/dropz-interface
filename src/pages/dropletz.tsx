5da875e8f8173cfcdf96a8a91c2e18140fa87287
Kai Morciniec <kaimorciniec@gmail.com>
2022-06-25 15:16:20 -0400

    add connect alert if user is not connected

--- src/pages/dropletz.tsx
+++ src/pages/dropletz.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import Layout from "../components/layout/Layout";
+import {
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription,
+} from "@chakra-ui/react";
 
 const Home: NextPage = () => {
     const { data: account } = useAccount();
-    if (!account) return <> Please connect </>;
+    if (!account)
+        return (
+            <>
+                {" "}
+                <Alert status="error">
+                    <AlertIcon />
+                    Please connect a wallet!
+                </Alert>{" "}
+            </>
+        );
     const dropRes = fetch(`/api/dropletz/${account?.address}`);
     return <>Dropletz</>;
 };
