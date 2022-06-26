ca06bf38c0548e45e5f758e31067631b6e62f70a
Kai Morciniec <kaimorciniec@gmail.com>
2022-06-26 03:35:29 -0400

    add untracked files

--- src/components/Worldcoin.tsx
+++ src/components/Worldcoin.tsx
@@ -1,12 +1,26 @@
 import { useLayoutEffect } from "react";
 import worldID from "@worldcoin/id"; // If you installed the JS package as a module
+import { useAccount } from "wagmi";
 
 export default function Worldcoin(): JSX.Element {
+    const { data: account } = useAccount();
+
     const enableWorldId = async () => {
         try {
             const { merkle_root, nullifier_hash, proof } =
                 await worldID.enable();
             console.log("World ID verified successfully:", nullifier_hash);
+            const postData = {
+                addr: account.address,
+                nullHash: nullifier_hash,
+            };
+            await fetch("/api/verify/1", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(postData),
+            });
         } catch (failure) {
             console.warn("World ID verification failed:", failure);
             //   const result = await worldID.enable();
@@ -17,7 +31,7 @@ export default function Worldcoin(): JSX.Element {
         if (!worldID.isInitialized()) {
             worldID.init("world-id-container", {
                 // enable_telemetry: true,
-                action_id: "wid_staging_aab43c65569959c2942f23cfffb71e4d",
+                action_id: "wid_staging_b03bdf58f69c3dc1be604d70c263205e",
                 signal: "any_string",
             });
         }
---
+++ src/pages/api/verify/[addr].tsx
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const verifiedAddrs: string[] = [];
+const verifiedNullHashes: string[] = [];
+export async function verifyAddress(addr: string, nullHash: string) {
+    console.log(verifiedNullHashes, verifiedNullHashes.includes(nullHash));
+    if (verifiedNullHashes.includes(nullHash)) {
+        throw "Person has already verified";
+    }
+    if (isVerifiedAddr(addr)) {
+        throw "Address has already been verified";
+    }
+    verifiedAddrs.push(addr);
+    verifiedNullHashes.push(nullHash);
+    console.log("Verified", addr);
+}
+export function isVerifiedAddr(addr: string): boolean {
+    return verifiedAddrs.includes(addr);
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === "POST") {
+        console.log("HII");
+        verifyAddress(req.body.addr, req.body.nullHash);
+        res.status(200).json({ 1: 1 });
+    } else if (req.method === "GET") {
+        const { addr } = req.query;
+        console.log(addr, verifiedAddrs);
+        const isVerified = isVerifiedAddr(addr);
+        res.status(200).json({ isVerified: isVerified });
+    }
+}
